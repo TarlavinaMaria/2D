a104bb3dcc300341d3464c827843ca546c04ddc7
tarla <Tarlavina50@mail.ru>
2025-06-05 23:16:25 +0700

    Описание кода

--- Assets/Data/Script2/Enemy.cs
+++ Assets/Data/Script2/Enemy.cs
@@ -7,22 +7,25 @@ public class Enemy : MonoBehaviour
 
     private void Awake()
     {
+        // Получаем компонент анимации врага
         _animator = GetComponent<Animator>();
     }
 
+    // Метод получения урона
     public void TakeDamage()
     {
-        _health--;
+        _health--; // Уменьшаем здоровье врага
         Debug.Log($"Враг получил урон! Осталось жизней: {_health}");
 
-        _animator.SetTrigger("Hit"); // Анимация удара
+        _animator.SetTrigger("Hit"); // Запускаем анимацию удара
 
+        // Если враг больше не имеет здоровья, вызываем метод смерти
         if (_health <= 0)
         {
             Die();
         }
     }
-
+    // Метод смерти врага
     private void Die()
     {
         Debug.Log("Враг уничтожен!");
--- Assets/Data/Script2/PlayerMove.cs
+++ Assets/Data/Script2/PlayerMove.cs
@@ -1,35 +1,42 @@
 using UnityEngine;
 
+// Требуемые компоненты: Спрайт, Анимация, Физика, Коллайдер
 [RequireComponent(typeof(SpriteRenderer))]
 [RequireComponent(typeof(Animator))]
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(BoxCollider2D))]
 public class PlayerMove : MonoBehaviour
 {
+    // Константы для анимационных параметров
     private const string Horizontal = nameof(Horizontal);
     private const string Speed = nameof(Speed);
     private const string Jump = nameof(Jump);
     private const string SpeedUpDown = nameof(SpeedUpDown);
     private const string AttackAnim = nameof(Attack);
 
+    // Настройки движения и прыжка
     [SerializeField] private float _speed;
     [SerializeField] private float _jumpForce;
     [SerializeField] private KeyCode _jumpKeyCode;
 
+    // Настройки атаки
     [SerializeField] private float _attackRange = 1f;
     [SerializeField] private KeyCode _attackKey = KeyCode.F;
     [SerializeField] private LayerMask _enemyLayer;
 
+    // Компоненты
     private Rigidbody2D _rigidbody2D;
     private BoxCollider2D _boxCollider2D;
     private Animator _animator;
     private SpriteRenderer _spriteRenderer;
+
+    // Переменные состояния
     private float _derection;
     private bool _isGround = false;
 
-
     private void Awake()
     {
+        // Получаем компоненты при старте
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _boxCollider2D = GetComponent<BoxCollider2D>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
@@ -38,68 +45,56 @@ public class PlayerMove : MonoBehaviour
 
     private void Update()
     {
+        // Управление движением персонажа
         _derection = Input.GetAxis(Horizontal) * _speed * Time.deltaTime;
         _rigidbody2D.velocity = new Vector2(_derection, _rigidbody2D.velocity.y);
 
+        // Проверяем нажатие клавиши прыжка
         if (Input.GetKeyDown(_jumpKeyCode) && _isGround == true)
         {
             _isGround = false;
             _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, _jumpForce);
-            // Анимация прыжка
             _animator.SetBool(Jump, true);
             _animator.SetFloat(SpeedUpDown, _rigidbody2D.velocity.y);
-
-            Flipx();// Поворот спрайта по направлению движения
+            Flipx();
         }
         else if (_rigidbody2D.velocity.y < 0)
         {
             _animator.SetFloat(SpeedUpDown, _rigidbody2D.velocity.y);
-            //_animator.SetBool(Jump, false);
         }
 
+        // Управление поворотом персонажа и анимациями движения
         if (_rigidbody2D.velocity.x > 0)
         {
             transform.Translate(_speed * Time.deltaTime, 0, 0);
-            _animator.SetFloat(Speed, 1); // Анимация движение, скорость
-
-            Flipx(); // Поворот спрайта по направлению движения
+            _animator.SetFloat(Speed, 1);
+            Flipx();
         }
         else if (_rigidbody2D.velocity.x < 0)
         {
             transform.Translate(_speed * Time.deltaTime * -1, 0, 0);
-            _animator.SetFloat(Speed, 1);// Анимация движение, скорость
-
-            Flipx(); // Поворот спрайта по направлению движения
+            _animator.SetFloat(Speed, 1);
+            Flipx();
         }
         else
         {
-            _animator.SetFloat(Speed, 0);// Анимация покоя, скорость
+            _animator.SetFloat(Speed, 0); // Покой
         }
+
+        // Проверка атаки
         if (Input.GetKeyDown(_attackKey))
         {
             Attack();
         }
     }
-    // Метод для поворота спрайта персонажа по направлению движения
+
+    // Метод для поворота спрайта персонажа в зависимости от направления
     private void Flipx()
     {
-        if (_derection > 0)
-        {
-            // Движение вправо - обычное отображение
-            _spriteRenderer.flipX = false;
-        }
-        else if (_derection < 0)
-        {
-            // Движение влево - зеркальное отображение
-            _spriteRenderer.flipX = true;
-        }
-        else
-        {
-            _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, _rigidbody2D.velocity.y);
-        }
-
+        _spriteRenderer.flipX = _derection < 0;
     }
-    // Метод для прыжка (колизия), работает при столкновении колайдером, но только 1 раз
+
+    // Метод обработки столкновения с землей
     private void OnCollisionEnter2D()
     {
         _isGround = true;
@@ -107,24 +102,22 @@ public class PlayerMove : MonoBehaviour
         _animator.SetFloat(Speed, 1);
         _animator.SetFloat(SpeedUpDown, 0);
     }
-    //private void OnCollisionStay2D()
-    //{
 
-    //}
-    //private void OnCollisionExit2D()
-    //{
-
-    //}
+    // Метод атаки персонажа
     private void Attack()
     {
         _animator.SetTrigger(AttackAnim); // Запускаем анимацию удара
         Debug.Log("Удар!");
 
+        // Определяем направление удара
         Vector2 attackDirection = _spriteRenderer.flipX ? Vector2.left : Vector2.right;
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, attackDirection, _attackRange, LayerMask.GetMask("Enemy"));
+
+        // Создаем луч для проверки попадания удара
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, attackDirection, _attackRange, _enemyLayer);
 
         if (hit.collider != null)
         {
+            // Если объект — враг, наносим ему урон
             Enemy enemy = hit.collider.GetComponent<Enemy>();
             if (enemy != null)
             {
