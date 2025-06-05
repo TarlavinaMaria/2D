1939514272ddbb56cc4929b71c5c8568ddcf6481
tarla <Tarlavina50@mail.ru>
2025-06-05 23:05:15 +0700

    Анимация

--- Assets/Data/Script2/Enemy.cs
+++ Assets/Data/Script2/Enemy.cs
@@ -3,12 +3,19 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private int _health = 3;
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
 
     public void TakeDamage()
     {
         _health--;
-
         Debug.Log($"Враг получил урон! Осталось жизней: {_health}");
+
+        _animator.SetTrigger("Hit"); // Анимация удара
 
         if (_health <= 0)
         {
@@ -19,6 +26,7 @@ public class Enemy : MonoBehaviour
     private void Die()
     {
         Debug.Log("Враг уничтожен!");
-        Destroy(gameObject);
+        _animator.SetTrigger("Die"); // Анимация смерти
+        Destroy(gameObject, 1f); // Удаление после короткой задержки
     }
 }
